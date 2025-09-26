c55496c3879af18ac029d128d3b6de8872eef546
“ThitapornL” <thitaporn.l@gofive.co.th>
2025-09-26 16:43:17 +0700

    fix : navbar

---
+++ lib/src/core/base/image_resource.dart
@@ -0,0 +1,19 @@
+class ImageResource {
+  static const String logo = 'assets/images/logo.png';
+  static const String placeholder = 'assets/images/placeholder.png';
+
+  //  navbar
+  static const String homeUnselected = 'assets/common/ic_home_unselect.svg'; 
+  static const String learnUnselected = 'assets/common/ic_learn_unselect.svg'; 
+  static const String newFeedUnselected = 'assets/common/ic_new_feed_unselect.svg'; 
+  static const String profileUnselected = 'assets/common/ic_profile_unselect.svg'; 
+  static const String swipeUnselected = 'assets/common/ic_swipe_unselect.svg'; 
+ 
+  static const String homeSelected = 'assets/common/ic_home.svg';
+  static const String learnSelected = 'assets/common/ic_learn.svg';
+  static const String newFeedSelected = 'assets/common/ic_new_feed.svg';
+  static const String profileSelected = 'assets/common/ic_profile.svg';
+  static const String swipeSelected = 'assets/common/ic_swipe.svg';
+
+
+}
